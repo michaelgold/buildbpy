a31f77ae13df8ad15b6d485a1e0b56e6e779b4e7
Michael Gold <goldmichael@gmail.com>
2023-11-30 11:19:27 -0500

    wip: build blender

---
+++ workspace/blender/build_blender.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+def build_blender():
+    # Run Blender's build commands
+    subprocess.check_call(['make', 'update'])
+    subprocess.check_call(['make', 'bpy'])
+
+    # Create wheel
+    os.chdir('build_files/utils')
+    subprocess.check_call(['python3', 'make_bpy_wheel.py', '../../build_darwin_bpy/bin/', '/wheel-output'])
+
+if __name__ == "__main__":
+    build_blender()
