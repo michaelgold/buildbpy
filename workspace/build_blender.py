7c66fd8e7aee8b073ee00dc7faad7d898c30547e
Michael Gold <goldmichael@gmail.com>
2023-12-01 00:52:28 -0500

    feat: app commands

--- workspace/build_blender.py
+++ workspace/build_blender.py
@@ -7,7 +7,7 @@ import dotenv
 import os
 import glob
 
-
+app = typer.Typer()
 # Load the environment variables
 dotenv.load_dotenv()
 github_token = os.getenv("GITHUB_TOKEN")
@@ -16,6 +16,79 @@ github_token = os.getenv("GITHUB_TOKEN")
 
 # get script directory
 
+@app.command()
+def publish_wheel(tag: str, whl_file_path: Path):
+    """ Publishes the wheel file to GitHub Releases. """
+    selected_tag = tag  
+    # https://docs.github.com/en/rest/reference/repos#create-a-release
+    headers = {
+    "Authorization": f"Bearer {github_token}",
+    "Accept": "application/vnd.github.v3+json"
+    }
+
+    # check if the release already exists
+    release_url = f"https://api.github.com/repos/michaelgold/bpy/releases/tags/{selected_tag}"
+    response = requests.get(release_url, headers=headers)
+    print(f"response code: {response.status_code}")
+    print(f"response json: {response.json()}")
+
+    if response.status_code == 200 or response.status_code == 201:
+        print("Release already exists. Skipping creation.")
+        response = requests.get(release_url, headers=headers)
+        response_json = response.json()
+        release_id = response_json.get("id", None)
+
+        release_assets = response_json.get("assets", [])
+        existing_asset = None
+        for asset in release_assets:
+            if asset["name"] == f"{whl_file_path.stem}.whl":
+                existing_asset = asset
+                break
+        
+        if existing_asset:
+            # Delete the existing asset
+            print("Deleting existing asset.")
+            asset_id = existing_asset["id"]
+            delete_url = f"https://api.github.com/repos/michaelgold/bpy/releases/assets/{asset_id}"
+            response = requests.delete(delete_url, headers=headers)
+            print(f"Asset deleted response code: {response.status_code}")
+        
+    else:
+        print("Creating release.")
+        # Step 1: Create the release
+
+        release_name = f"gold-bpy-{selected_tag}"
+        release_body = f"Blender Python API for Blender {selected_tag}"
+        release_tag = f"gold-bpy-{selected_tag}"
+        release_url = f"https://api.github.com/repos/michaelgold/bpy/releases"
+        release_data = {
+            "tag_name": selected_tag,
+            "target_commitish": "main",
+            "name": release_name,
+            "body": release_body,
+            "draft": False,
+            "prerelease": False
+        }
+        response = requests.post(release_url, headers=headers, json=release_data)
+
+
+        if response.status_code != 200 and response.status_code != 201:
+            raise Exception(f"Failed to get release info: {response.text}")
+
+        release_id = response.json()['id']
+
+    # Step 2: Upload the .whl file
+    upload_url = f"https://uploads.github.com/repos/michaelgold/bpy/releases/{release_id}/assets?name={whl_file_path.stem}.whl"
+    headers['Content-Type'] = 'application/octet-stream'
+
+    with open(whl_file_path, 'rb') as file:
+        data = file.read()
+        upload_response = requests.post(upload_url, headers=headers, data=data)
+
+    if upload_response.status_code not in [200, 201]:
+        raise Exception(f"Failed to upload asset: {upload_response.text}")
+
+    print("File uploaded successfully.")
 
 def check_new_tag(tag: str = None):
     repo_url = "https://api.github.com/repos/blender/blender"
@@ -93,76 +166,7 @@ def check_new_tag(tag: str = None):
         print(f"Wheel file found: {whl_file_path}")
 
 
-        # publish the wheel to github releases
-        # https://docs.github.com/en/rest/reference/repos#create-a-release
-        headers = {
-        "Authorization": f"Bearer {github_token}",
-        "Accept": "application/vnd.github.v3+json"
-        }
-
-        # check if the release already exists
-        release_url = f"https://api.github.com/repos/michaelgold/bpy/releases/tags/{selected_tag}"
-        response = requests.get(release_url, headers=headers)
-        print(f"response code: {response.status_code}")
-        print(f"response json: {response.json()}")
-
-        if response.status_code == 200 or response.status_code == 201:
-            print("Release already exists. Skipping creation.")
-            response = requests.get(release_url, headers=headers)
-            response_json = response.json()
-            release_id = response_json.get("id", None)
-
-            release_assets = response_json.get("assets", [])
-            existing_asset = None
-            for asset in release_assets:
-                if asset["name"] == f"{whl_file_path.stem}.whl":
-                    existing_asset = asset
-                    break
-            
-            if existing_asset:
-                # Delete the existing asset
-                print("Deleting existing asset.")
-                asset_id = existing_asset["id"]
-                delete_url = f"https://api.github.com/repos/michaelgold/bpy/releases/assets/{asset_id}"
-                response = requests.delete(delete_url, headers=headers)
-                print(f"Asset deleted response code: {response.status_code}")
-            
-        else:
-            print("Creating release.")
-            # Step 1: Create the release
-
-            release_name = f"gold-bpy-{selected_tag}"
-            release_body = f"Blender Python API for Blender {selected_tag}"
-            release_tag = f"gold-bpy-{selected_tag}"
-            release_url = f"https://api.github.com/repos/michaelgold/bpy/releases"
-            release_data = {
-                "tag_name": selected_tag,
-                "target_commitish": "main",
-                "name": release_name,
-                "body": release_body,
-                "draft": False,
-                "prerelease": False
-            }
-            response = requests.post(release_url, headers=headers, json=release_data)
-
-
-            if response.status_code != 200 and response.status_code != 201:
-                raise Exception(f"Failed to get release info: {response.text}")
-
-            release_id = response.json()['id']
-
-        # Step 2: Upload the .whl file
-        upload_url = f"https://uploads.github.com/repos/michaelgold/bpy/releases/{release_id}/assets?name={whl_file_path.stem}.whl"
-        headers['Content-Type'] = 'application/octet-stream'
-
-        with open(whl_file_path, 'rb') as file:
-            data = file.read()
-            upload_response = requests.post(upload_url, headers=headers, data=data)
-
-        if upload_response.status_code not in [200, 201]:
-            raise Exception(f"Failed to upload asset: {upload_response.text}")
-
-        print("File uploaded successfully.")
+        publish_wheel(selected_tag, whl_file_path)
         
 
 
@@ -175,8 +179,8 @@ def check_new_tag(tag: str = None):
         
         return True
    
-
-def main(tag: str = typer.Option(None, help="Specific tag to check out")):
+@app.command()
+def build(tag: str = typer.Option(None, help="Specific tag to check out")):
     """
     This script checks for new tags in the Blender repository on GitHub.
     If a new tag is found, or a specific tag is provided, it updates the local repository and a data file.
@@ -185,4 +189,4 @@ def main(tag: str = typer.Option(None, help="Specific tag to check out")):
     check_new_tag(tag)
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
